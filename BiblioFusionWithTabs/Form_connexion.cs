ec488924c62601fc8b6ddcd6c4fe5722bc2ab2c4
bodina <leo.razafindralambo@gmail.com>
2026-03-04 17:42:46 +0100

    TextesBox et formulaire de connexion actualisés

--- BiblioFusionWithTabs/Form_connexion.cs
+++ BiblioFusionWithTabs/Form_connexion.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BiblioFusionWithTabs.Controls;
 
 namespace WindowsFormsApp1
 {
@@ -15,11 +16,45 @@ namespace WindowsFormsApp1
         public ConnexionForm()
         {
             InitializeComponent();
+
+            var mainFont = new Font(Name, 30, FontStyle.Regular);
+
             Label titleLabel = new Label()
             {
                 Text = "BiblioFusion",
-                Font = new Font(Name, 24, FontStyle.Bold)
+                AutoSize = true,
+                Font = mainFont,
+                TextAlign = ContentAlignment.MiddleCenter
             };
+            
+            Label idLabel = new Label()
+            {
+                Text = "Identifiant :",
+                AutoSize = true,
+                Font = mainFont,
+                Location = new Point(20, 120)
+            };
+
+            Label mdpLabel = new Label()
+            {
+                Text = "Mot de passe :",
+                AutoSize = true,
+                Font = mainFont,
+                Location = new Point(20, 220)
+            };
+
+            TextBox idTextBox = new TextBox
+            {
+                Location = new Point(20, 170), Size = new Size(300, 30)
+            };
+
+            TextBox mdpTextBox = new TextBox
+            {
+                Location = new Point(20, 170),
+                Size = new Size(300, 30)
+            };
+
+            Controls.AddRange(new Control[]{ titleLabel, idTextBox, idLabel, mdpTextBox, mdpLabel  });
         }
     }
 }
--- BiblioFusionWithTabs/MainForm.cs
+++ BiblioFusionWithTabs/MainForm.cs
@@ -17,13 +17,29 @@ namespace BiblioFusionWithTabs
         public MainForm()
         {
             InitializeComponent();
+
             ConnexionForm formConnexion = new ConnexionForm();
             formConnexion.Show();
             var mainMenuStrip = new MainMenustrip();
             var mainTabControl = new MainTabControl();
-            mainTabControl.TabPages.AddRange(new TabPage[] { new TabPage("Tableau De Bord"), new TabPage("Rechercher"), new TabPage("Scanner"), new TabPage("Statistiques") });
 
-            mainTabControl.TabPages[0].Controls.Add(new Label() { Text = "BiblioFusion - Tableau de Bord", AutoSize = true, Font = new Font(Name, 16, FontStyle.Bold), Location = new Point(20, 20) });
+            mainTabControl.TabPages.AddRange(new TabPage[]
+            { 
+                new TabPage("Tableau De Bord"),
+                new TabPage("Rechercher"),
+                new TabPage("Scanner"),
+                new TabPage("Statistiques") 
+            });
+
+            mainTabControl.TabPages[0].Controls.AddRange(new Control[]
+            {
+
+                (new Label() { Text = "BiblioFusion - Tableau de Bord", AutoSize = true, Font = new Font(Name, 16, FontStyle.Bold), Location = new Point(20, 20) }),
+                (new ListView() { Location = new Point(20, 60), Size = new Size(200, 400), View = View.Details, Columns = { "Retard" }, Items = { new ListViewItem(new string[] { "150" }) } }),
+                (new ListView() { Location = new Point(270, 60), Size = new Size(200, 400), View = View.Details, Columns = { "Retours" }, Items = { new ListViewItem(new string[] { "150" }) } }),
+                (new ListView() { Location = new Point(520, 60), Size = new Size(200, 400), View = View.Details, Columns = { "Réservations" }, Items = { new ListViewItem(new string[] { "150" }) } })
+
+            });
             mainTabControl.TabPages[1].Controls.Add(new Label() { Text = "BiblioFusion - Rechercher", AutoSize = true, Font = new Font(Name, 16, FontStyle.Bold), Location = new Point(20, 20) });
             mainTabControl.TabPages[2].Controls.Add(new Label() { Text = "BiblioFusion - Scanner", AutoSize = true, Font = new Font(Name, 16, FontStyle.Bold), Location = new Point(20, 20) });
             mainTabControl.TabPages[3].Controls.Add(new Label() { Text = "BiblioFusion - Statistiques", AutoSize = true, Font = new Font(Name, 16, FontStyle.Bold), Location = new Point(20, 20) });
